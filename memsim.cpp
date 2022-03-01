524cdbc8aaab69865b77173a6a032dd907c34892
Airick73 <72322466+Airick73@users.noreply.github.com>
2022-03-01 09:15:53 -0500

    Add files via upload

--- memsim.cpp
+++ memsim.cpp
@@ -94,7 +94,7 @@ void fifo(vector<pageTableEntry> pageTable, int nframes){
 		if (frameTable.size() < nframes){ //if the frameTable is not fulll
 			if (frameTable.find(pageTable[i].address)==frameTable.end()){ //if the desired page is not in the frame table
 				if(pageTable[i].rw == 'R'){//read not in the frame table .... count as read and load in page table
-					reads++; //incrementing global variable reads
+                    reads++; //incrementing global variable reads
 					frameTable.insert(pageTable[i].address); //inserting into frameTable
 					q.push(pageTable[i].address);
 				}
@@ -102,25 +102,41 @@ void fifo(vector<pageTableEntry> pageTable, int nframes){
 					reads++; //incrementing global variable reads
 					pageTable[i].dirty = 1; //marking as dirty
 					frameTable.insert(pageTable[i].address); //inserting into frameTable
-					q.push(pageTable[i].address);
+					q.push(pageTable[i].address); //pushing to q
 				}
 			}
+            else{//frameTable is not full and desired page is in the frame table
+                if(pageTable[i].rw == 'W'){
+                    pageTable[i].dirty = 1;
+                }
+            } 
 		}
 		else{ //if the frame table is full
 			if (frameTable.find(pageTable[i].address) == frameTable.end()){ //the desired page is not in the full frame table
 				reads++; //if the page is not in frame table then we are adding it and regaurdless of it is a W or R we still count a read
 				int val = q.front(); //val is = to the front of the queue
-				//find the corresponding page that contains the address of val and check if it was dirty
+
+				//val is the address that needs to be evicted
 				//	if it was dirty then count it as a write 
-				for(int i = 0; i < 1000000; i++){
-					if(pageTable[i].address == val && pageTable[i].dirty == 1)
-						writes++;
-				} 
+                //going through the pageTable 
+                //  if the matching address to val is also dirty count as write
+				for(int k = 0; k < i; k++){
+                    if(pageTable[k].address == val && pageTable[k].dirty == 1){
+                         writes++;
+                         k = 1000000;
+                    }
+
+                }
 				q.pop(); //removing the page at the front of the queue
 				frameTable.erase(val); //removing page that was located at the front of the q from the page table
 				frameTable.insert(pageTable[i].address); //inserting the new desired page into the newest spot in the frameTable
 				q.push(pageTable[i].address); //inserting the new desired page into the back of the queue
 			}
+            else{//frameTable is full and desired page is in the frame table
+                if(pageTable[i].rw == 'W'){
+                    pageTable[i].dirty = 1;
+                }
+            } 
 		}
 	}
 }
